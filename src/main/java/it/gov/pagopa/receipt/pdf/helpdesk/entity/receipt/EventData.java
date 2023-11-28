ec833426b6ccc8c742bccc2b9b98189b31841f3b
giomella <gioele.mella.st@everis.nttdata.com>
2023-11-28 12:50:38 +0100

    [PRDP-237] added missing field in Receipt entity

--- src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/entity/receipt/EventData.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/entity/receipt/EventData.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.receipt.pdf.helpdesk.entity.receipt;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +11,8 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class EventData {
     private String payerFiscalCode;
     private String debtorFiscalCode;
--- src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/entity/receipt/ReasonError.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/entity/receipt/ReasonError.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.receipt.pdf.helpdesk.entity.receipt;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ReasonError {
     private int code;
     private String message;
