4169d5746ce362503ca4cf73c624357151ca1d77
svariant <samuele.varianti@nttdata.com>
2023-12-04 12:42:44 +0100

    [PRDP-257] Added download attachment method to blob client

--- src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/client/ReceiptBlobClient.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/client/ReceiptBlobClient.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.receipt.pdf.helpdesk.client;
 
+import it.gov.pagopa.receipt.pdf.helpdesk.exception.BlobStorageClientException;
 import it.gov.pagopa.receipt.pdf.helpdesk.model.response.BlobStorageResponse;
 
+import java.io.File;
 import java.io.InputStream;
 
 public interface ReceiptBlobClient {
 
     BlobStorageResponse savePdfToBlobStorage(InputStream pdf, String fileName);
+    File getAttachmentFromBlobStorage(String fileName) throws BlobStorageClientException;
 }
