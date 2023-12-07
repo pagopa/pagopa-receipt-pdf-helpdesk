4a8cb8b0223a86d48aee860f180f501d769ad6fd
svariant <samuele.varianti@nttdata.com>
2023-12-07 18:30:06 +0100

    [PRDP-242] Fix typo

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -5,6 +5,6 @@
 cd ./src || exit
 yarn install
 yarn test:"$1" || true
-echo "Sleeping for 300 seconds (5 minuts) before running teardown"
+echo "Sleeping for 300 seconds (5 minutes) before running teardown"
 sleep 300
 yarn teardown:"$1"
