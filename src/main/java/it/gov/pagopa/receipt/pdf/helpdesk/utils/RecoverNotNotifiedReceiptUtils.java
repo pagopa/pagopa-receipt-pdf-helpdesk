813b087eac23b3717e7f62bc7ca0e843e32af87a
pasqualespica <36746022+pasqualespica@users.noreply.github.com>
2024-01-17 11:22:49 +0100

    fix

--- src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/utils/RecoverNotNotifiedReceiptUtils.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/helpdesk/utils/RecoverNotNotifiedReceiptUtils.java
@@ -1,19 +1,14 @@
 package it.gov.pagopa.receipt.pdf.helpdesk.utils;
 
 import com.azure.cosmos.models.FeedResponse;
-import it.gov.pagopa.receipt.pdf.helpdesk.RecoverNotNotifiedReceipt;
 import it.gov.pagopa.receipt.pdf.helpdesk.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.helpdesk.entity.receipt.enumeration.ReceiptStatusType;
 import it.gov.pagopa.receipt.pdf.helpdesk.service.ReceiptCosmosService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecoverNotNotifiedReceiptUtils {
-
-    private static final Logger logger = LoggerFactory.getLogger(RecoverNotNotifiedReceiptUtils.class);
 
     public static Receipt restoreReceipt(Receipt receipt) {
         receipt.setStatus(ReceiptStatusType.GENERATED);
@@ -31,12 +26,10 @@ public class RecoverNotNotifiedReceiptUtils {
 
     public static List<Receipt> receiptMassiveRestore(ReceiptStatusType statusType, ReceiptCosmosService receiptCosmosService) {
 
-        logger.info("RecoverNotNotified Massive Restore for statusType " + statusType + "executing");
 
         List<Receipt> receiptList = new ArrayList<>();
         String continuationToken = null;
         do {
-            logger.info("RecoverNotNotified Massive Restore executing recover cycle on status " + statusType);
 
             Iterable<FeedResponse<Receipt>> feedResponseIterator =
                     receiptCosmosService.getNotNotifiedReceiptByStatus(continuationToken, 100, statusType);
@@ -47,12 +40,9 @@ public class RecoverNotNotifiedReceiptUtils {
                     receiptList.add(restoredReceipt);
                 }
                 continuationToken = page.getContinuationToken();
-                logger.info("RecoverNotNotified Massive Restore continuationToken : " + continuationToken);
 
             }
         } while (continuationToken != null);
-
-        logger.info("RecoverNotNotified Massive Restore for statusType " + statusType + "executed");
 
 
         return receiptList;
